1f9dac0020c599887897e81a00a52b02f83d2b38
Phillip <71596849+Philliip@users.noreply.github.com>
2023-04-23 00:57:04 +0200

    updated asgi

--- sellit_api/asgi.py
+++ sellit_api/asgi.py
@@ -8,17 +8,20 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
+
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
-
+django_asgi_app = get_asgi_application()
 from apps.chat import routing
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sellit_api.settings.development')
+django.setup()
 
-django_asgi_app = get_asgi_application()
+
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
