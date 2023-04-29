f390d9527ccf3277fb4fa724b5859c46331ff793
Phillip <71596849+Philliip@users.noreply.github.com>
2023-04-29 21:53:36 +0200

    push notifications

--- apps/core/models/expo_token.py
+++ apps/core/models/expo_token.py
@@ -3,6 +3,12 @@ from apps.core.models import User
 from apps.core.models.base import BaseModel
 
 class ExpoToken(BaseModel):
+
+    class Meta:
+        app_label = 'core'
+        db_table = 'expo_tokens'
+        default_permissions = ()
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     token = models.CharField(max_length=255, unique=True)
 
